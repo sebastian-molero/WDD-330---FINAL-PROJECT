e9c1a190dc9bad1ce2e16a3e9a216e9a44e0866d
sebastian-molero <smoleronavarro@byupathway.edu>
2025-12-02 12:36:00 -0500

    route fix

--- scripts/utils.mjs
+++ scripts/utils.mjs
@@ -4,16 +4,18 @@ export async function loadHeaderFooter() {
     toggleNav();
 }
 
+const basePath = window.location.pathname.includes("/") ? "../" : "./";
+
 async function loadHeader() {
     const header = document.getElementById("header");
-    const response = await fetch("../partials/header.html");
+    const response = await fetch(`${basePath}partials/header.html`);
     const html = await response.text();
     header.innerHTML = html;
 }
 
 async function loadFooter() {
     const footer = document.getElementById("footer");
-    const response = await fetch("../partials/footer.html");
+    const response = await fetch(`${basePath}partials/footer.html`);
     const html = await response.text();
     footer.innerHTML = html;
 }
